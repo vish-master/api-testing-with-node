9a1777ad0f14af09e49fcf83dcd3de9d63a72b36
dima.vyshnyvetskiy@outlook.com <dima.vyshnyvetskiy@outlook.com>
2022-01-13 14:10:48 +0200

    fix to pipeline

---
+++ jenkins/JenkinsFile.groovy
@@ -0,0 +1,53 @@
+node {
+    def app
+
+    stage("Build App") {
+        buildApp()
+    }
+    stage("Run App") {
+        startApp()
+    }
+    stage('Test App') {
+        try {
+            testApp()
+        }catch(Exception e){
+            println e.getMessage()
+            throw e
+        }finally{
+            killAppAfterTest()
+        }
+    }
+
+    stage('Build image') {
+        app = docker.build("19972909/node-js")
+    }
+
+    stage('Push image') {
+        docker.withRegistry('https://registry.hub.docker.com', 'git') {
+            app.push("${env.BUILD_NUMBER}")
+            app.push("latest")
+        }
+    }
+}
+
+def buildApp(){
+    sh 'npm ci'
+}
+
+def startApp() {
+    sh ' set -x'
+    sh ' kill $(cat .pidfile)'
+    sh ' set +x'
+}
+
+def testApp() {
+    sh ' set - x'
+    sh 'npm test'
+    sh 'set + x'
+}
+
+def killAppAfterTest() {
+    set - x
+    kill $(cat.pidfile)
+    set + x
+}
